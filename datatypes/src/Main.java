3d5a17899b843dfe6016c173e1b151e473a88d18
santaluric <114261276+santaluric@users.noreply.github.com>
2022-09-25 12:12:38 +0300

    Add files via upload

---
+++ datatypes/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+       // int integer olup tam sayı ile kullanılır
+        int sayi = 12 ;
+       // int -2 milyar ve +2 milyar arasında yazılır
+        // long  int den daha büyük sayıları kullanmakta kullanılır ve 8 byt yer tutar
+        // ondalık sayı kullanılır ise double kullanılır
+        // float  daha az ondalık sayıları yazmada kullanılır
+        sayi = 11 ;
+
+        char karakter = 'A';
+        String cokluKarakter = "ANKARA";
+        // ascii kodu  A  a kodları ver karşılığı sayısal
+
+        boolean dogruMu = false ;
+
+
+    }
+}
